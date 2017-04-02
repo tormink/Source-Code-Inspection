327b5f09cd611c35ba2f2c5d040a685b76e1b144
Lucas Tormin <lucasntormin@gmail.com>
2017-04-02 18:05:14 -0300

    Atributos agora são privados. (#1)

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -10,9 +10,9 @@ import java.util.Iterator;
  */
 public class TicketMachine {
 
-    protected int valor;
-    protected int saldo;
-    protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    private int valor;
+    private int saldo;
+    private int[] papelMoeda = {2, 5, 10, 20, 50, 100};
 
     public TicketMachine(int valor) {
         this.valor = valor;
